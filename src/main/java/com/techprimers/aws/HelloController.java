2d50dcd00d47e2e6b82426b741b67fb2b0fc4207
badari-narayana.s@capgemini.com <badari-narayana.s@capgemini.com>
2020-06-01 20:26:51 +0200

    build spec fileadded

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -1,11 +1,13 @@
 package com.techprimers.aws;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    @GetMapping
     public String hello(){
         return "Hellow YouTube Codepipeline";
     }
