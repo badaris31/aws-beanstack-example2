9c709d9d763f3a41093806c28113ed1f4e9d51cc
badari-narayana.s@capgemini.com <badari-narayana.s@capgemini.com>
2020-06-01 18:48:56 +0200

    AWS bean stack example code commit

---
+++ src/main/java/com/techprimers/aws/AwsElasticbeanstackExample2Application.java
@@ -0,0 +1,13 @@
+package com.techprimers.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstackExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticbeanstackExample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimers/aws/AwsElasticbeanstackExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticbeanstackExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
